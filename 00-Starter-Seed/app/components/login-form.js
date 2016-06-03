4c2a8ae060e2d685a96a668ecaef3d83e326ce25
mehreencs87 <mehreen.cs87@gmail.com>
2016-06-03 15:24:08 +0500

    seed

---
+++ 00-Starter-Seed/app/components/login-form.js
@@ -0,0 +1,26 @@
+/* global Auth0Lock */
+
+import Ember from 'ember';
+import config from 'start/config/environment';
+
+const { service } = Ember.inject;
+
+var lock = new Auth0Lock(config.Auth0.clientId, config.Auth0.domain);
+
+export default Ember.Component.extend({
+  didRender: function() {
+    this._super(...arguments);
+    lock.show({
+        callbackURL: 'http://localhost:4200/callback'
+      , responseType: 'token'
+      , authParams: {
+        scope: 'openid email'  // Learn about scopes: https://auth0.com/docs/scopes
+      }
+    });
+
+  },
+
+  willDestroyElement: function() {
+    lock.hide();
+  }
+});
---
+++ 00-Starter-Seed/config/environment.js
@@ -0,0 +1,53 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'start',
+    environment: environment,
+    baseURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    },
+
+    Auth0: {
+      clientId: 'm2g7qIaQJngPtHOs5zl4bEHsVrSywa7W',
+      domain: 'ntotten-demo.auth0.com',
+      callbackUrl: 'http://localhost:4200/callback',
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
